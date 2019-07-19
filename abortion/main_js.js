aa61b0ab7110827254cb0995efd21b81dafe171c
zohaqamar <zoha.qamar@nytimes.com>
2019-07-19 17:22:29 -0400

    timeline updates

--- abortion/main_js.js
+++ abortion/main_js.js
@@ -1,1 +1,23 @@
-text("Hello world!");
+import 'intersection-observer';
+import scrollama from 'scrollama'; // or...
+const scrollama = require('scrollama');
+
+
+
+// instantiate the scrollama
+const scroller = scrollama();
+
+// setup the instance, pass callback functions
+scroller
+  .setup({
+    step: '.step'
+  })
+  .onStepEnter(response => {
+    // { element, index, direction }
+  })
+  .onStepExit(response => {
+    // { element, index, direction }
+  });
+
+// setup resize event
+window.addEventListener('resize', scroller.resize);
